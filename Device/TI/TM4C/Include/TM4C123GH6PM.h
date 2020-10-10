3b5c66f9e40e5b05bc3b38acb252433aa80cfa48
Nhan <dtnhan57@gmail.com>
2020-10-10 20:02:51 +0700

    Update struct GPIOA_Type
    __IO  uint32_t  DATA_Bits[255];                   /*!< Bit specific data registers

--- Device/TI/TM4C/Include/TM4C123GH6PM.h
+++ Device/TI/TM4C/Include/TM4C123GH6PM.h
@@ -229,7 +229,7 @@ typedef struct {                                    /*!< WATCHDOG0 Structure    
   */
 
 typedef struct {                                    /*!< GPIOA Structure                                                       */
-  __I  uint32_t  RESERVED0[255];
+  __IO  uint32_t  DATA_Bits[255];                   /*!< Bit specific data registers                                           */
   __IO uint32_t  DATA;                              /*!< GPIO Data                                                             */
   __IO uint32_t  DIR;                               /*!< GPIO Direction                                                        */
   __IO uint32_t  IS;                                /*!< GPIO Interrupt Sense                                                  */
